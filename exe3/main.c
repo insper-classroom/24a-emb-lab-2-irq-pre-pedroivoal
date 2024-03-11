7013d372023fdbcbba27302f2dfac8a9017c1d21
pedroivoal <pedroial@al.insper.edu.br>
2024-03-11 17:59:34 +0000

    ex3

--- exe3/main.c
+++ exe3/main.c
@@ -5,12 +5,15 @@
 const int BTN_PIN_R = 28;
 const int BTN_PIN_G = 26;
 
+volatile int PRESSED_R = 0;
+volatile int PRESSED_G = 0;
+
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge
     if (gpio == BTN_PIN_R)
-      printf("fall red\n");
+      PRESSED_R = 1;
     else if (gpio == BTN_PIN_G)
-      printf("fall green\n");
+      PRESSED_G = 1;
   }
 }
 
@@ -33,5 +36,13 @@ int main() {
   gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
   while (true) {
+    if (PRESSED_R) {
+      printf("fall red\n");
+      PRESSED_R = 0;
+    }
+    else if (PRESSED_G) {
+      printf("fall green\n");
+      PRESSED_G = 0;
+    }
   }
 }
